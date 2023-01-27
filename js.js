349d202fea9c06c3b01c1fedfcd22ffc70d4bf8d
Igor-Lysak <lisakigor485@gmail.com>
2023-01-27 22:58:22 +0200

    dz

---
+++ js.js
@@ -0,0 +1,21 @@
+let zapyt = prompt("How many digits will be in the massive: ")
+let massive = [];
+
+for(let i = 0; i < zapyt; i++){
+    massive.push(Math.floor(Math.random() * 100)); 
+    console.log(massive);
+}
+
+
+
+
+// function ArraySum(numbers){
+//     let sum = 0;
+//     for(let i = 0; i < numbers.length; i++){
+//         sum += numbers[i];
+//     }
+//     return sum;
+// }
+
+// ArraySum();
+
